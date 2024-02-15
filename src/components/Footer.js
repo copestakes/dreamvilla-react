290effd0b9a5673ef4442bf7bde2647ea51d6f54
David Copestakes <davecopestakes@gmail.com>
2024-02-15 12:59:52 -0500

    footer and rating section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,64 @@
 import React from 'react';
+import logo from '../images/dreamvillas-logo.svg';
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <section>
+        <div className="company-info">
+          <img src={logo} alt="logo" />
+          <ul>
+            <li>
+              Address: <br /> 1001 Walnut Street, Philadephia PA
+            </li>
+            <li>
+              Phone: <br /> 1.800.555.1122
+            </li>
+
+            <li>
+              Email: <br /> dreamhome@dreamvillas.com
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h3>Important Links</h3>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/">Property</a>
+            </li>
+            <li>
+              <a href="/">Listed</a>
+            </li>
+            <li>
+              <a href="/">Services</a>
+            </li>
+            <li>
+              <a href="/">Contact</a>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h3>Social Media</h3>
+          <ul>
+            <li>
+              <a href="/">Facebook</a>
+            </li>
+            <li>
+              <a href="/">Instagram</a>
+            </li>
+            <li>
+              <a href="/">Twitter</a>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/Ratings.js
+++ src/components/Ratings.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAward } from '@fortawesome/free-solid-svg-icons';
+import { faHouseChimney } from '@fortawesome/free-solid-svg-icons';
+import { faFaceSmileWink } from '@fortawesome/free-solid-svg-icons';
 
 function Ratings() {
   return (
@@ -9,16 +13,19 @@ function Ratings() {
       </h1>
       <div className="rating-section">
         <div className="rating-card">
+          <FontAwesomeIcon className="rating-icon" icon={faAward} />
           <h1>30+</h1>
           <p>Service Awards</p>
         </div>
 
         <div className="rating-card">
+          <FontAwesomeIcon className="rating-icon" icon={faHouseChimney} />
           <h1>5000+</h1>
           <p>Homes for Sale</p>
         </div>
 
         <div className="rating-card">
+          <FontAwesomeIcon className="rating-icon" icon={faFaceSmileWink} />
           <h1>2000+</h1>
           <p>Happy Customers</p>
         </div>
